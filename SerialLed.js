de802ce0b0dd2b00d1f0dfa2dd039b0c69553a1d
david <Davidfritz04@gmail.com>
2021-12-07 23:02:15 +0100

    SerialLed finally works

--- SerialLed.js
+++ SerialLed.js
@@ -3,50 +3,72 @@ const chroma = require("chroma-js")
 class SerialLed //Class Name
 {
 
-    constructor(node){
-        this.node=node
-this.sending=false;
+    constructor(node) {
+        this.node = node
+        this.sending = false;
 
-        this.serial = new SerialPort(this.node.port,{baudRate:this.node.baudRate})
+
+
+this.serial=undefined
+     //   this.serial = new SerialPort(this.node.port, {baudRate: this.node.baudRate, autoOpen: false})
+
+        setTimeout(()=>(this.serial = new SerialPort(this.node.port, {baudRate: this.node.baudRate, autoOpen: true})),1000)
+
     }
     update() {
         this.msg_parse(this.node.data.data.domain([0, this.node.num_leds]), this.node.data.brightness).then(async (msg) => {
 
-if(!this.sending){
 
+if(!this.sending && this.serial!==undefined) {
 this.sending=true
-for(let i = 0; i<Math.ceil(msg.length/20);i++){
 
-    let tmp = '<'+i*20+","+ msg.slice(i*60,i*60+60).toString()+'>'
-    console.log(tmp)
-    setTimeout(
-        ()=>{this.serial.write(tmp, function (err) {
+    for (let j = -8; j < this.node.num_leds; j++) {
 
-        if (err) {
-            return console.log('Error on write: ', err.message)
+        let tmp = ""
+        if(j>0) {
+            tmp += "/" + j
+            for (let k = 0; k < 3; k++) {
+                tmp += ("," + msg[3 * j + k])
+            }
+        }
+        else{
+            tmp += "/" + j+",0,0,0"
         }
 
-    })
-            if(i+1===Math.ceil(msg.length/20)){
-                this.sending=false
-            }
-        },i*50)
+            this.serial.write(tmp, function (err) {
+
+                if (err) {
+                    return console.log('Error on write: ', err.message)
+                }
+
+            })
+
+
+    }
+    this.serial.drain(()=>this.sending=false)
 }
 
 
 
+
+
+
+
+
+
+
+    })
+}
+    updateArgs() {
+        if (this.serial !== undefined) {
+            this.serial.close(function (err) {
+                if (err) {
+                    return console.log('Error on write: ', err.message)
+                }
+            })
+            this.serial = new SerialPort(this.node.port, {baudRate: parseInt(this.node.baudRate)})
         }
-    })
-}
-    updateArgs(){
-        this.serial.close(function (err) {
-            if (err) {
-                return console.log('Error on write: ', err.message)
-            }
-    })
-        this.serial=new SerialPort(this.node.port,{baudRate:parseInt(this.node.baudRate)})
-}
-
+    }
     msg_parse (scale, brightness=1) {
         let out = []
         return new Promise((resolve) => {
